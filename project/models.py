3a0fb4c5285a1fc90a4afa9cc88a39f5f1fe9680
Will Connell <wconnell93@gmail.com>
2020-10-12 15:14:26 -0700

    multi film network

--- project/models.py
+++ project/models.py
@@ -103,56 +103,56 @@ class FiLMNetwork(pl.LightningModule):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
 
     
-# class FiLMNetwork(pl.LightningModule):
-#     def __init__(self, inputs_sz, conds_sz, learning_rate=1e-3, batch_size=2048, metric=r2_score):
-#         super().__init__()
-#         self.save_hyperparameters()
-#         self.metric = metric
-#         self.inputs_emb = LinearBlock(in_sz=inputs_sz, layers=[512,256,128,64], out_sz=32, ps=None, use_bn=True, bn_final=True)
-#         self.conds_emb = LinearBlock(in_sz=conds_sz, layers=[], out_sz=32, ps=None, use_bn=True, bn_final=False)
-#         self.film_1 = FiLMGenerator(in_sz=self.conds_emb.out_sz, layers=[], out_sz=32, ps=None, use_bn=False, bn_final=False)
-#         self.block_1 = LinearBlock(in_sz=self.film_1.out_sz, layers=[16], out_sz=16, ps=None, use_bn=True, bn_final=True)
-#         self.film_2 = FiLMGenerator(in_sz=self.conds_emb.out_sz, layers=[], out_sz=16, ps=None, use_bn=False, bn_final=False)
-#         self.block_2 = LinearBlock(in_sz=self.film_2.out_sz, layers=[8], out_sz=1, ps=None, use_bn=True, bn_final=False)
+class MultiFiLMNetwork(pl.LightningModule):
+    def __init__(self, inputs_sz, conds_sz, learning_rate=1e-3, batch_size=2048, metric=r2_score):
+        super().__init__()
+        self.save_hyperparameters()
+        self.metric = metric
+        self.inputs_emb = LinearBlock(in_sz=inputs_sz, layers=[512,256,128,64], out_sz=32, ps=None, use_bn=True, bn_final=True)
+        self.conds_emb = LinearBlock(in_sz=conds_sz, layers=[], out_sz=32, ps=None, use_bn=True, bn_final=False)
+        self.film_1 = FiLMGenerator(in_sz=self.conds_emb.out_sz, layers=[], out_sz=32, ps=None, use_bn=False, bn_final=False)
+        self.block_1 = LinearBlock(in_sz=self.film_1.out_sz, layers=[16], out_sz=16, ps=None, use_bn=True, bn_final=True)
+        self.film_2 = FiLMGenerator(in_sz=self.conds_emb.out_sz, layers=[], out_sz=16, ps=None, use_bn=False, bn_final=False)
+        self.block_2 = LinearBlock(in_sz=self.film_2.out_sz, layers=[8], out_sz=1, ps=None, use_bn=True, bn_final=False)
     
-#     def forward(self, inputs, conds_a, conds_b):
-#         inputs_emb = self.inputs_emb(inputs)
-#         conds_a_emb = self.conds_emb(conds_a)
-#         conds_b_emb = self.conds_emb(conds_b)
-#         gamma_1, beta_1 = self.film_1(conds_a_emb)
-#         x = inputs_emb * gamma_1 + beta_1
-#         x = self.block_1(x)
-#         gamma_2, beta_2 = self.film_2(conds_b_emb)
-#         x = x * gamma_2 + beta_2
-#         y_hat = self.block_2(x)
-#         y_hat = torch.clamp(y_hat, min=0)
-#         return inputs_emb, conds_a_emb, conds_b_emb, y_hat
+    def forward(self, inputs, conds_a, conds_b):
+        inputs_emb = self.inputs_emb(inputs)
+        conds_a_emb = self.conds_emb(conds_a)
+        conds_b_emb = self.conds_emb(conds_b)
+        gamma_1, beta_1 = self.film_1(conds_a_emb)
+        x = inputs_emb * gamma_1 + beta_1
+        x = self.block_1(x)
+        gamma_2, beta_2 = self.film_2(conds_b_emb)
+        x = x * gamma_2 + beta_2
+        y_hat = self.block_2(x)
+        y_hat = torch.clamp(y_hat, min=0)
+        return inputs_emb, conds_a_emb, conds_b_emb, y_hat
 
-#     def training_step(self, batch, batch_idx):
-#         inputs, conds, y = batch
-#         inputs_emb, conds_a_emb, conds_b_emb, y_hat = self.forward(inputs, conds, conds)
-#         loss = F.mse_loss(y_hat, y)
-#         self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-#         return loss
+    def training_step(self, batch, batch_idx):
+        inputs, conds, y = batch
+        inputs_emb, conds_a_emb, conds_b_emb, y_hat = self.forward(inputs, conds, conds)
+        loss = F.mse_loss(y_hat, y)
+        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
     
-#     def validation_step(self, batch, batch_idx):
-#         inputs, conds, y = batch
-#         inputs_emb, conds_a_emb, conds_b_emb, y_hat = self.forward(inputs, conds, conds)
-#         loss = F.mse_loss(y_hat, y)
-#         self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-#         self.log('val_r2', self.metric(y.detach().cpu(), y_hat.detach().cpu()), on_step=False, on_epoch=True, prog_bar=True, logger=True)
-#         return loss
+    def validation_step(self, batch, batch_idx):
+        inputs, conds, y = batch
+        inputs_emb, conds_a_emb, conds_b_emb, y_hat = self.forward(inputs, conds, conds)
+        loss = F.mse_loss(y_hat, y)
+        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log('val_r2', self.metric(y.detach().cpu(), y_hat.detach().cpu()), on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
 
-#     def test_step(self, batch, batch_idx):
-#         inputs, conds, y = batch
-#         inputs_emb, conds_a_emb, conds_b_emb, y_hat = self.forward(inputs, conds, conds)
-#         loss = F.mse_loss(y_hat, y)
-#         self.log('test_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-#         self.log('test_r2', self.metric(y.detach().cpu(), y_hat.detach().cpu()), on_step=False, on_epoch=True, prog_bar=True, logger=True)
-#         return loss
+    def test_step(self, batch, batch_idx):
+        inputs, conds, y = batch
+        inputs_emb, conds_a_emb, conds_b_emb, y_hat = self.forward(inputs, conds, conds)
+        loss = F.mse_loss(y_hat, y)
+        self.log('test_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log('test_r2', self.metric(y.detach().cpu(), y_hat.detach().cpu()), on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return loss
 
-#     def configure_optimizers(self):
-#         return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
 
 
 class ConcatNetwork(pl.LightningModule):
